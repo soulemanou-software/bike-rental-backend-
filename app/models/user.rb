8eb098c63137dacd671c0b96ee12e4766d4e9c87
Soulemanou Kunchiefuh Babanou <kunchiefuh@gmail.com>
2023-05-20 21:02:41 +0200

    setup

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-  has_many :reservations, dependent: :destroy
-  has_many :bikes, dependent: :destroy
-
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, length: { minimum: 6 }
-end
